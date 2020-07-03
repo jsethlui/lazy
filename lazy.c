777344a39825d73e26c1eb47de6eb5b94445f2d2
Jeremy Louie <43184783+jsethlui@users.noreply.github.com>
2020-07-02 23:12:02 -0700

    Added cpp option
    Still some logic bug issues present

--- lazy.c
+++ lazy.c
@@ -15,26 +15,30 @@ int main(int argc, const char* argv[]) {
 
 	char* file;
 	int opt;
-	int py_flag, c_flag;
+	int py_flag, c_flag, cpp_flag;
 	py_flag = 0;
 	c_flag = 0;
 
 	static struct option long_options[] = {
 		{"py", no_argument, NULL, 'p'},
-		{"c", no_argument, NULL, 'c'},
+		{"c", no_argument, NULL, 'C'},
+		{"cpp", no_argument, NULL, 'c'},
 		{NULL, 0, NULL, 0}
 	};
 
-	while ((opt = getopt_long(argc, argv, "p:c:", long_options, NULL)) != -1) {
+	while ((opt = getopt_long(argc, argv, "p:C:c:", long_options, NULL)) != -1) {
 		switch (opt) {
 			case 'p':
 				py_flag = 1;
 				file = optarg;
 				break;
-			case 'c':
+			case 'C':
 				c_flag = 1;
 				file = optarg;
 				break;
+			case 'c':
+				cpp_flag = 1;
+				file = optarg;
 			default:
 				printf("\tdefault");
 				break;
@@ -43,15 +47,29 @@ int main(int argc, const char* argv[]) {
 
 	// if py flag is set
 	if (py_flag == 1) {
-		const char* py_header = "def main()\n" \
-								"    print(\"Hello World\")\n\n" \
-								"if __name__ == \"__main__\":\n" \
+		const char* py_main = "def main()\n" 
+								"    print(\"Hello World\")\n\n" 
+								"if __name__ == \"__main__\":\n" 
 								"    main()\n";
-		write_to_file(file, py_header);
+		write_to_file(file, py_main);
 	}
 
 	// if c flag is set
 	if (c_flag == 1) {
+		const char* c_main = "#include <stdio.h>\n\n" 
+							"int main(int argc, const char * argv[])\n" 
+							"    printf(\"Hello World\")" 
+							"    return 0\n" 
+							"}\n";
+		printf("%s", c_main);
+	}
 
+	if (cpp_flag == 1) {
+		const char* cpp_main = "#include <iostream>\n\n" 
+							"int main(int argc, const char * argv[]) {\n" 
+							"    std::cout << \"Hello World\")" 
+							"    return 0\n" 
+							"}\n";
+		printf("%s", cpp_main);
 	}
 }
